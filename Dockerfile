51dae76045f3f2a25b70949db9ea702b6e66be03
greivin <gcruzr@gmail.com>
2020-07-13 14:52:44 -0600

    Primer version Visor Ceniga - Nginx-leaflet

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
-FROM nginx:1.9
-MAINTAINER James Badger <james@jamesbadger.ca>
+FROM nginx:1.18-alpine
 
-ADD ./preconfigure.sh /preconfigure.sh
+LABEL maintainer="TI GEOINN greivin.cruz@geoinn.com"
+
+#ADD ./preconfigure.sh /preconfigure.sh
+
+COPY html /usr/share/nginx/html
+
 COPY dist /usr/share/nginx/html
 
-CMD ./preconfigure.sh
+#CMD ./preconfigure.sh
+CMD nginx -g "daemon off;"
+
--- preconfigure.sh
+++ preconfigure.sh
@@ -17,11 +17,15 @@ cat <<HERE >/usr/share/nginx/html/index.html
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Leaflet</title>
+  <title>CENIGA Visor Geoespacial - Leaflet</title>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  
+ 
 
-  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.css" />
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin=""/>
+ 
   <style>
     body {
       padding: 0px;
@@ -38,12 +42,14 @@ cat <<HERE >/usr/share/nginx/html/index.html
 <body>
   <div id="map"></div>
 
-  <script src="//cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.3/leaflet.js"></script>
+<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin=""></script>
+
+
   <script src="//cdnjs.cloudflare.com/ajax/libs/zepto/1.1.4/zepto.min.js"></script>
   <script src="autosize.js"></script>
   <script>
     Autosize.enable();
-    var map = L.map('map').setView([43.6000,1.433333], 10);
+    var map = L.map('map').setView([9.5, -84], 8);
 
     L.tileLayer('${TILESERVER}', {
       attribution: '${ATTRIBUTION}'
